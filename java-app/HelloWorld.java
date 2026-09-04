46709758590c364965ed3bf73d6a71972cfe3633
Tejaswini-vmv <tejaswini.folane@vmvinfotech.com>
2026-09-05 00:01:57 +0530

    Add Java Docker application

---
+++ java-app/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Docker!");
+        System.out.println("My Fourth Dockerized Application");
+        System.out.println("Application: Java");
+    }
+}
